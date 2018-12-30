1f51bff1c71c24cdb917c61cb7de92525dff2710
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-30 14:39:11 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
 counter = 0
 block_return_values = []
 while counter < collection.length
-block_return_values << yield (collection[counter])
-  counter += 1
-end
+    block_return_values << yield(collection[counter])
+    counter += 1
+  end
 end
